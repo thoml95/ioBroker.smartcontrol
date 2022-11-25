74a16a436f775a2c793246783a19e9ae4bd3e05f
Christian Oelschlegel <18003795+oelison@users.noreply.github.com>
2022-11-25 22:27:22 +0100

    removed got
    * removed got
    * added axios as replacement
    * contact modified
    
    #19 solved, #18 solved

--- main.js
+++ main.js
@@ -49,9 +49,7 @@ class SmartControl extends utils.Adapter {
             Trigger:   require('./lib/trigger-class.js'), // Class for Triggers and Target devices handling
             mSuncalc:  require('suncalc2'),               // https://github.com/andiling/suncalc2
             mSchedule: require('node-schedule'),          // https://github.com/node-schedule/node-schedule
-            //ToDo: need to be replaced by axios
-            mGot:      require('@esm2cjs/got'),           // https://github.com/sindresorhus/got || This is a 'request' replacement due to https://nodesource.com/blog/express-going-into-maintenance-mode#alternativestorequest
-
+            axios:     require('axios').default,
             // {object} timers    - All timer objects.
             timersZoneOn: {},    // for option "onAfter" in Zones table
             timersZoneOff: {},    // for option "offAfter" in Zones table
@@ -809,7 +807,7 @@ class SmartControl extends utils.Adapter {
                     try {
                         this.log.debug(`Trying to call target URL (name: '${name}', URL: '${url}')`);
                         // @ts-ignore This expression is not callable.
-                        const response = await this.x.mGot(url); // This is a 'request' replacement due to https://nodesource.com/blog/express-going-into-maintenance-mode#alternativestorequest
+                        const response = await this.x.axios.get(url);
                         this.log.debug(`Calling target URL was successful (name: '${name}', URL: '${url}')`);
                         // Set response to state smartcontrol.x.targetURLs.xxxxxxx.response
                         if (this.x.helper.isLikeEmpty(response.body)) {
